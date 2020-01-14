64ca56ef0860a7bed6909a5e6f756bf90d52425a
fernandotanno <fernando.tanno@gmail.com>
2020-01-14 00:14:54 -0300

    Seed de usuario default

---
+++ database/seeds/UserSeeder.js
@@ -0,0 +1,14 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+
+class UserSeeder {
+  static async run () {
+    await Factory
+    .model('App/Models/User')
+    .create()
+  }
+}
+
+module.exports = UserSeeder
